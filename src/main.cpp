26c53841811c8955260ea94c411793ba09f602fa
Alexandre Quessy <alexandre@quessy.net>
2010-07-19 10:05:45 -0400

    trying to catch an error

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 //#include <cvaux.h>
 #include <highgui.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <cv.h>
 //#include <cxcore.h>
 //#include <cxcore.h>
@@ -17,16 +18,27 @@ int main(int argc, char *argv[]) {
     int output_height = 128;
     if (verbose)
         std::cout << "Loading image " << input_image_path << std::endl;
-    IplImage* ipl = cvLoadImage(input_image_path.c_str());
+    IplImage* input_image = cvLoadImage(input_image_path.c_str());
+    if (!input_image) {
+        std::cout << "Could not load image file " << input_image_path << std::endl;
+        exit(1);
+    }
+    CvSize size = cvSize(output_width, output_height);
     if (verbose)
         std::cout << "Resizing image to " << output_width << "x" << output_height << std::endl;
-    CvSize size = cvSize(output_width, output_height);
 
-    IplImage* tmpsize = cvCreateImage(size, 8, 0);
-    cvResize(ipl, tmpsize, CV_INTER_LINEAR);
+    //IplImage* output_image = cvCreateImage(size, 8, 0);
+    IplImage* output_image = cvCreateImage(size, IPL_DEPTH_32S, 3);
+    try {
+        cvResize(input_image, output_image, CV_INTER_LINEAR);
+    } catch (std::exception &e) { // cv::Exception &e) {
+        //const char* error_message = e.what();
+        std::cout << e.what() << std::endl;
+        exit(1);
+    }
     if (verbose)
         std::cout << "Saving image as " << output_image_path << std::endl;
-    cvReleaseImage(&ipl);
+    cvReleaseImage(&input_image);
 
     return 0;
 }
